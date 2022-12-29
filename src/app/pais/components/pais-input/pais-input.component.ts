231a968e5d9db31be3222925cae1ef2ee1f4a850
Antonio Galisteo <agaliste@bancolombia.com.co>
2022-12-29 08:27:55 -0500

    Debounce

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pais-input',
@@ -8,12 +10,24 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class PaisInputComponent implements OnInit {
 
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();
 
   termino: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.debouncer
+    .pipe(debounceTime(300))
+    .subscribe( valor => {
+      this.onDebounce.emit(valor);
+    });
+  }
+
+  teclaPresionada() {
+    this.debouncer.next(this.termino);
   }
 
   buscar() {

37d94ca1d3cf8f7d9cf983db6be4334b41c14ed6
Antonio Galisteo <agaliste@bancolombia.com.co>
2022-12-30 08:07:25 -0500

    Search by capital country

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
@@ -9,6 +9,7 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class PaisInputComponent implements OnInit {
 
+  @Input() placeholder: string = "";
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
   @Output() onDebounce: EventEmitter<string> = new EventEmitter();
 
--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorCapitalComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital(this.termino).subscribe(paises => {
+      this.paises = paises;
+      console.log(paises);
+    }, (err) => {
+      this.hayError = true;
+      this.paises = [];
+    });
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    // TODO crear sugerencias
   }
 
 }
